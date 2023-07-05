9e4ab749aa369e6b78f21d9710baed85cc16733c
halil <halilakgun0@gmail.com>
2023-07-05 16:40:27 +0300

    Initial commit

---
+++ src/main/java/com/ozgur/laboratoryreportingapplication/LaboratoryReportingApplication.java
@@ -0,0 +1,13 @@
+package com.ozgur.laboratoryreportingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryReportingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozgur/laboratoryreportingapplication/LaboratoryReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozgur.laboratoryreportingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
